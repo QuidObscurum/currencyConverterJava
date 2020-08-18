e3170de99a31cdb0339ace81197fdd6f9ca29413
QuidObscurum <quid.obscurum.quid.divinum@gmail.com>
2020-08-18 19:34:09 +0300

    Refactor config

--- src/main/java/com/quid/currencyconverter/config/ApplicationConfig.java
+++ src/main/java/com/quid/currencyconverter/config/ApplicationConfig.java
@@ -1,7 +1,14 @@
 package com.quid.currencyconverter.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@ComponentScan("com.quid.currencyconverter")
+@ComponentScan(basePackages = {
+        "com.quid.currencyconverter.controller",
+        "com.quid.currencyconverter.dao",
+        "com.quid.currencyconverter.dbservice",
+        "com.quid.currencyconverter.service"
+})
+@Configuration
 public class ApplicationConfig {
 }
